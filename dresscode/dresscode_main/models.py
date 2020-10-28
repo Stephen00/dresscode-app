507eeb1b42bdbcdf758f69d9d62bc42e16de137a
Jose Luis Povedano Poyato <2403203P@student.gla.ac.uk>
2020-10-28 11:50:28 +0000

    Models 1 and settings updated

--- dresscode/dresscode_main/models.py
+++ dresscode/dresscode_main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tags(models.Model):
+    tag=models.CharField(max_length=128)
+
+class QuizQuestions(models.Model):
+    question=models.TextField()
+    answer=models.TextField()
+    other1=models.TextField()
+    other2=models.TextField()
+    other3=models.TextField()
+
+class Quizzes(models.Model):
+    questions=models.ManyToManyField(QuizQuestions)
+    tags=models.ManyToManyField(Tags)
