a55a75580f2dfdfb5f7cef3c28b177689aa43428
Jose Luis Povedano Poyato <2403203P@student.gla.ac.uk>
2020-10-30 14:28:34 +0000

    Created some of the more complicated Models

--- dresscode/dresscode_main/models.py
+++ dresscode/dresscode_main/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Tags(models.Model):
@@ -16,7 +18,7 @@ class QuizQuestions(models.Model):
     other1 = models.TextField()
     other2 = models.TextField()
     other3 = models.TextField()
-
+    
 
 class Quizzes(models.Model):
     questions = models.ManyToManyField(QuizQuestions)
@@ -31,9 +33,32 @@ class Poll(models.Model):
     answer = models.TextField()
     tags = models.ManyToManyField(Tags)
 
+
 class Article(models.Model):
     title = models.TextField()
     media1 = models.ForeignKey(Media, null=True, on_delete=models.SET_NULL)
     paragraph = models.TextField()
     tags = models.ManyToManyField(Tags)
     
+    
+class Post(models.Model):
+    author=models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    description=models.TextField()
+    reaction1_counter=models.IntegerField()
+    reaction2_counter=models.IntegerField()
+    reaction3_counter=models.IntegerField()
+    content_type=models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content=GenericForeignKey('content_type', 'object_id')
+    
+    def post_type(self):
+        return self.content_type.name
+        
+    def title(self):
+        if self.content.question:
+            return self.content.question
+        else:
+            return self.content.title
+            
+    def full_title(self):
+        return self.post_type()+" : "+self.title()
