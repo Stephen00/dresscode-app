f93003c8cd2fa6d4f598ef0a9d72e9ad87a50ced
Martina Borisova <2382419b@student.gla.ac.uk>
2020-11-18 16:57:20 +0200

    Test connection between backend and fronted
     * Display article titles on homepage
     * Make App.tsx into class component
    
    NB
     * Only works with running backend and populated DB

--- dresscode/client-app/src/App.tsx
+++ dresscode/client-app/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8000/discover/articles/").then((response) => {
+      this.setState({
+        articles: response.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.articles.map((article: any) => (
+              <li key={article.pk}>{article.title}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
