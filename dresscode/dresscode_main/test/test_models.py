0876c944c6bdee26415e9a97e1089b7f5eddef23
Stephen Graham <2385583g@student.gla.ac.uk>
2020-11-18 16:26:28 +0000

    added incomplete test_models

---
+++ dresscode/dresscode_main/test/test_models.py
@@ -0,0 +1,74 @@
+from django.test import TestCase
+
+from dresscode_main.models import Poll, Article, Tag, QuizQuestion
+
+
+class testAttributes(TestCase):
+
+    def setUp(self):
+        # Initialise Tags
+        tags = ['Java', 'Databases', 'C', 'Python', 'Algorithms', 'Sigma16', 'Deep Neural Networks', 'WebApp',
+                'Game Dev', 'Back-End', 'Front-End', 'Threading', 'Django', 'AJAX', 'React', 'SQL',
+                'Functional Programming', 'Machine Learning', 'C++']
+        for t in tags:
+            tag = Tag(tag=t)
+            tag.save()
+
+        # Create tasks to be used in test data
+        java_tag = Tag.objects.get(tag="Java")
+        c_tag = Tag.objects.get(tag="C")
+        python_tag = Tag.objects.get(tag="Python")
+
+        # Create test poll
+        p = Poll.objects.create(media=None, question="What is your favourite programming language?", answer1="Python",
+                                answer2="Java", answer3="C++", counter1=7, counter2=9, counter3=11)
+        p.save()
+
+        # Create test articles
+        a = Article.objects.create(media1=None, title="Why Java is awesome",
+                                   paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sem mattis, consequat mi quis, vulputate felis. Phasellus vitae lobortis diam. Proin dapibus est sapien, eget bibendum lacus vehicula at. Maecenas nisl diam, placerat vel quam in, interdum maximus arcu. Aenean quis leo in orci laoreet ullamcorper at nec lectus.")
+        a.save()
+
+        # Create test  quiz question
+        qq = QuizQuestion.objects.create(media=None,
+                                         question="What is the correct way to declare an integer variable equal to 1 in C#",
+                                         answer="int var = 1;", other1="var = 1", other2="int var =1",
+                                         other3="int var ==1;")
+        qq.save()
+
+    # Test Cases here
+    def testPollQuestion(self):
+        test_poll = Poll.objects.all()[0]
+        self.assertEquals(test_poll.question, "What is your favourite programming language?")
+
+    def testPollReaction(self):
+        test_poll = Poll.objects.all()[0]
+
+        self.assertEquals(test_poll.counter1, 7)
+
+    def testPollAnswer(self):
+        test_poll = Poll.objects.all()[0]
+        test_poll.vote_poll("Python")
+        self.assertEquals(test_poll.answer1, "Python")
+
+    def testArticleTitle(self):
+        test_article = Article.objects.all()[0]
+        self.assertEquals(test_article.title, "Why Java is awesome")
+
+    def testArticleParagraph(self):
+        test_article = Article.objects.all()[0]
+        self.assertEquals(test_article.paragraph, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a sem "
+                                                  "mattis, consequat mi quis, vulputate felis. Phasellus vitae lobortis "
+                                                  "diam. Proin dapibus est sapien, eget bibendum lacus vehicula at. "
+                                                  "Maecenas nisl diam, placerat vel quam in, interdum maximus arcu. "
+                                                  "Aenean quis leo in orci laoreet ullamcorper at nec lectus.")
+
+    def testQuizQuestion(self):
+        test_question = QuizQuestion.objects.all()[0]
+        self.assertEquals(test_question.question,
+                          "What is the correct way to declare an integer variable equal to 1 in C#")
+
+    def testQuizQuestionAnswer(self):
+        test_question = QuizQuestion.objects.all()[0]
+        #print(test_question.check_answer("int var = 1;"))
+        self.assertEquals(test_question.answer, "int var = 1;")
