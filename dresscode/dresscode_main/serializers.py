ed2aba35e02684db709607b03a3e166c160d1be1
Stephen Graham <2385583g@student.gla.ac.uk>
2020-12-18 19:47:47 +0000

    added media serailizer references

--- dresscode/dresscode_main/serializers.py
+++ dresscode/dresscode_main/serializers.py
@@ -3,35 +3,45 @@ from .models import Quiz, QuizQuestion, Post, Tag, Article, Media, Poll
 
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Media 
+        model = Media
         fields = ('pk', 'video', 'image')
-        
+
 class QuizQuestionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = QuizQuestion 
+        model = QuizQuestion
+        media = MediaSerializer()
         fields = ('pk', 'media', 'question', 'answers', 'tags')
-        
+
+
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Quiz 
+        model = Quiz
         fields = ('pk', 'questions', 'tags', 'slug')
-        
+
+
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Poll 
+        model = Poll
+        media = MediaSerializer()
         fields = ('pk', 'question', 'media', 'answers', 'slug')
-        
+
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Tag 
+        model = Tag
         fields = ('pk', 'tag')
-       
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Article 
+        model = Article
+        media = MediaSerializer()
         fields = ('pk', 'title', 'media', 'text', 'tags', 'slug')
-        
+
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Post 
-        fields = ('pk', 'author', 'description', 'reaction1_counter', 'reaction2_counter', 'reaction3_counter', 'content', 'content_type', 'object_id', 'tags')
+        model = Post
+        fields = (
+        'pk', 'author', 'description', 'reaction1_counter', 'reaction2_counter', 'reaction3_counter', 'content',
+        'content_type', 'object_id', 'tags')
