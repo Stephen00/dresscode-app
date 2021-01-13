2592383654c528f75c0458d6fd42bb7413928d8a
Thanatad Songpetchmongkol <2605415S@student.gla.ac.uk>
2021-01-13 20:57:19 +0700

    Refractor code, add new page (Discover Poll Page)

--- dresscode/client-app/src/features/navbar/NavBar.tsx
+++ dresscode/client-app/src/features/navbar/NavBar.tsx
@@ -34,33 +34,6 @@ const NavBar = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-
-    // <Navbar fixed="top" collapseOnSelect expand="lg">
-    // <Col lg={4} xs={9}>
-    //   <Navbar.Brand as={Link} to="/">
-    //     <img src="/assets/logo.svg" alt="dressCode logo" />
-    //   </Navbar.Brand>
-    // </Col>
-    // <Col lg={8} xs={3}>
-    //   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    //   <Navbar.Collapse id="responsive-navbar-nav">
-    //     <Nav>
-    //       <Nav.Link as={Link} to="/">
-    //         Latest
-    //       </Nav.Link>
-    //       <Nav.Link as={Link} to="/discover/articles">
-    //         Discover
-    //       </Nav.Link>
-    //       <Nav.Link as={Link} to="/">
-    //         People
-    //       </Nav.Link>
-    //       <Nav.Link as={Link} to="/">
-    //         More
-    //       </Nav.Link>
-    //     </Nav>
-    //   </Navbar.Collapse>
-    // </Col>
-    // </Navbar>
   );
 };
 
---
+++ dresscode/client-app/src/views/discover-poll-page/discover-poll-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './discover-poll-page.css';
+
+const DiscoverPollPage = () => {
+    return (
+        <div>
+            <h1>Discover Poll Page</h1>
+        </div>
+    )
+}
+
+export default DiscoverPollPage;
