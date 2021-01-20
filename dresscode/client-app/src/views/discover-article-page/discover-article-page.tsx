1caa41022cc215a130943233fed0bba0a3dfca98
Thanatad Songpetchmongkol <2605415S@student.gla.ac.uk>
2021-01-20 18:51:48 +0700

    sidebar semantic-ui trial

--- dresscode/client-app/src/views/discover-article-page/discover-article-page.tsx
+++ dresscode/client-app/src/views/discover-article-page/discover-article-page.tsx
@@ -5,9 +5,20 @@ import Picture from "../../assets/shutterstock_256173265_edit.jpg";
 import "./discover-article-page.css";
 import { Card, Col, Container, Nav, Row } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import {
+  Checkbox,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+} from 'semantic-ui-react'
 
 const DiscoverArticlePage = () => {
   const [articles, setArticles] = useState<IArticle[]>([]);
+  const [visible, setVisible] = React.useState(false)
 
   useEffect(() => {
     axios
@@ -22,7 +33,96 @@ const DiscoverArticlePage = () => {
 
   return (
     <div>
-      <Container>
+      <Grid columns={1}>
+        <Grid.Column>
+            <Checkbox
+            checked={visible}
+            label={{ children: <code>visible</code> }}
+            onChange={(e, data: any) => setVisible(data.checked)}
+            />
+        </Grid.Column>
+
+        <Grid.Column>
+            <Sidebar.Pushable as={Segment}>
+            <Sidebar
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                inverted
+                onHide={() => setVisible(false)}
+                vertical
+                visible={visible}
+                width='thin'
+            >
+                <Menu.Item as='a'>
+                  <Icon name='home' />
+                  <LinkContainer to="/discover/articles">
+                    <Nav.Link>Articles</Nav.Link>
+                  </LinkContainer>
+                </Menu.Item>
+                <Menu.Item as='a'>
+                  <Icon name='gamepad' />
+                  <LinkContainer to="/discover/quizzes">
+                    <Nav.Link>Quizzes</Nav.Link>
+                  </LinkContainer>
+                </Menu.Item>
+                <Menu.Item as='a'>
+                  <Icon name='camera' />
+                  <LinkContainer to="/discover/polls">
+                    <Nav.Link>Polls</Nav.Link>
+                  </LinkContainer>
+                </Menu.Item>
+            </Sidebar>
+
+            <Sidebar.Pusher>
+            {articles.map((article: any) => (
+              <Card key={article.pk}>
+                <Card.Title
+                  className="date-style text-color"
+                >
+                  21/10/2020
+                </Card.Title>
+                <Card.Title className="text-color">{article.title}</Card.Title>
+                <div className="image-section">
+                  <img
+                    src={Picture}
+                    alt="no picture found"
+                    className="overview-image"
+                  />
+                </div>
+                <Card.Body>
+                  <Row>
+                    <Col xs={4} className="icon-style">
+                      <span className="text-color">
+                        <i className="far fa-heart fa-2x">
+                          <span>25</span>
+                        </i>
+                      </span>
+                    </Col>
+                    <Col xs={4} className="icon-style">
+                      <span className="text-color">
+                        <i className="far fa-star fa-2x">
+                          <span>25</span>
+                        </i>
+                      </span>
+                    </Col>
+                    <Col xs={4} className="icon-style">
+                      <span className="text-color">
+                        <i className="far fa-share-square fa-2x">
+                          <span>4</span>
+                        </i>
+                      </span>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            ))}
+            </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </Grid.Column>
+        </Grid>
+        
+      {/* <Container>
         <Row>
           <Col xs={12} sm={12} md={2} lg={2}>
             <Nav defaultActiveKey="/home" className="flex-column">
@@ -32,7 +132,7 @@ const DiscoverArticlePage = () => {
               <LinkContainer to="/discover/quizzes">
                 <Nav.Link>Quizzes</Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/discover/quizzes">
+              <LinkContainer to="/discover/polls">
                 <Nav.Link>Polls</Nav.Link>
               </LinkContainer>
             </Nav>
@@ -82,7 +182,7 @@ const DiscoverArticlePage = () => {
             ))}
           </Col>
         </Row>
-      </Container>
+      </Container> */}
     </div>
   );
 };
--- dresscode/client-app/src/views/discover-poll-page/discover-poll-page.tsx
+++ dresscode/client-app/src/views/discover-poll-page/discover-poll-page.tsx
@@ -1,10 +1,67 @@
 import React from "react";
 import './discover-poll-page.css';
+import {
+    Checkbox,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    Menu,
+    Segment,
+    Sidebar,
+  } from 'semantic-ui-react'
 
 const DiscoverPollPage = () => {
+    const [visible, setVisible] = React.useState(false)
+
     return (
         <div>
             <h1>Discover Poll Page</h1>
+        <Grid columns={1}>
+        <Grid.Column>
+            <Checkbox
+            checked={visible}
+            label={{ children: <code>visible</code> }}
+            onChange={(e, data: any) => setVisible(data.checked)}
+            />
+        </Grid.Column>
+
+        <Grid.Column>
+            <Sidebar.Pushable as={Segment}>
+            <Sidebar
+                as={Menu}
+                animation='overlay'
+                icon='labeled'
+                inverted
+                onHide={() => setVisible(false)}
+                vertical
+                visible={visible}
+                width='thin'
+            >
+                <Menu.Item as='a'>
+                <Icon name='home' />
+                Home
+                </Menu.Item>
+                <Menu.Item as='a'>
+                <Icon name='gamepad' />
+                Games
+                </Menu.Item>
+                <Menu.Item as='a'>
+                <Icon name='camera' />
+                Channels
+                </Menu.Item>
+            </Sidebar>
+
+            <Sidebar.Pusher>
+            {/* <h1>Hello world</h1>
+                <Segment basic>
+                <Header as='h3'>Application Content</Header>
+                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+                </Segment> */}
+            </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </Grid.Column>
+        </Grid>
         </div>
     )
 }
