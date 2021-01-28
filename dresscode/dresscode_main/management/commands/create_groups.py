9570cf36c913bf7f1edc305f36f52d5a387800a0
Stephen Graham <2385583g@student.gla.ac.uk>
2021-01-28 15:31:01 +0000

    Updated to allow for different permissions in each user group

--- dresscode/dresscode_main/management/commands/create_groups.py
+++ dresscode/dresscode_main/management/commands/create_groups.py
@@ -3,19 +3,28 @@ from django.contrib.auth.models import Group, Permission
 
 import logging
 
+
+GROUPS_PERMISSIONS = {
+    'staff': ['view', 'change'],
+    'admin': ['view', 'delete', 'add', 'change']
+}
 groups = ['staff', 'admin']
-models = ['post', 'poll', 'article', 'quiz', 'quiz question']
+models = ['post', 'poll', 'article', 'quiz']
 permissions = ['view', 'delete', 'add', 'change'] # List of permissions for each group
+
 
 # a simple command to create the models with their associated permissions
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for group in groups:
+
+        for group in GROUPS_PERMISSIONS:
             print("Creating permissions for " + group)
             new_group, created = Group.objects.get_or_create(name=group)
+            # Delete old permissions, to prevent removing permissions not updating correctly
+            new_group.permissions.clear()
             for model in models:
-                for permission in permissions:
+                for permission in GROUPS_PERMISSIONS[group]:
                     name = 'Can {} {}'.format(permission, model)
                     # Try creating the permission, otherwise return an error
                     try:
