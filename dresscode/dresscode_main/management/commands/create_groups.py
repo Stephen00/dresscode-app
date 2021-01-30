ab1932afb9377723bd50fd4726a6bc98c5069683
Stephen Graham <2385583g@student.gla.ac.uk>
2021-01-30 19:19:55 +0000

    removed redundant parameters and fixed the except case

--- dresscode/dresscode_main/management/commands/create_groups.py
+++ dresscode/dresscode_main/management/commands/create_groups.py
@@ -1,16 +1,13 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Group, Permission
-
 import logging
 
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
 
 GROUPS_PERMISSIONS = {
     'staff': ['view', 'change'],
     'admin': ['view', 'delete', 'add', 'change']
 }
-groups = ['staff', 'admin']
-models = ['post', 'poll', 'article', 'quiz']
-permissions = ['view', 'delete', 'add', 'change'] # List of permissions for each group
+models = ['post', 'poll', 'article', 'quiz', 'Quiz Question']
 
 
 # a simple command to create the models with their associated permissions
@@ -19,7 +16,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         for group in GROUPS_PERMISSIONS:
-            print("Creating permissions for " + group)
+            print("Creating permissions for " + group + "...")
             new_group, created = Group.objects.get_or_create(name=group)
             # Delete old permissions, to prevent removing permissions not updating correctly
             new_group.permissions.clear()
@@ -29,8 +26,8 @@ class Command(BaseCommand):
                     # Try creating the permission, otherwise return an error
                     try:
                         perm = Permission.objects.get(name=name)
-                    except Permission.DoesNotExist:
-                        logging.warning("Permission not found: " + name)
+                    except perm.DoesNotExist:
+                        logging.warning(" Permission not found: " + name)
                         continue
                     new_group.permissions.add(perm)
         print("Permission groups created\n")
