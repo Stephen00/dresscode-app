642004e0d7c4673cab830f439939a2ad4a757fa0
Martina Borisova <2382419b@student.gla.ac.uk>
2021-02-10 17:42:10 +0000

    Removed homepage test as it's not valid anymore

--- dresscode/client-app/src/app/layout/App.test.tsx
+++ dresscode/client-app/src/app/layout/App.test.tsx
@@ -2,14 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { Router } from "react-router-dom";
-import HomePage from "../../views/home-page/home-page";
 import App from "./App";
-
-test("renders homepage text", () => {
-  render(<HomePage />);
-  const linkElement = screen.getByText("Home Page");
-  expect(linkElement).toBeInTheDocument();
-});
 
 test("landing on a bad page", () => {
   const history = createMemoryHistory();
