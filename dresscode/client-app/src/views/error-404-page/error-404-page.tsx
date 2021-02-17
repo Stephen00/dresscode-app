ca947e7b50438a95a933ec82b42de081f683d914
Thanatad Songpetchmongkol <2605415S@student.gla.ac.uk>
2021-02-17 14:46:33 +0700

    fix 404-page button

--- dresscode/client-app/src/views/error-404-page/error-404-page.tsx
+++ dresscode/client-app/src/views/error-404-page/error-404-page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './error-404-page.css';
 import { Button } from "react-bootstrap";
 import CatPicture from "../../assets/cat.png";
+import {Route} from 'react-router-dom'
 
 const Error404Page = () => {
 
@@ -13,9 +14,11 @@ const Error404Page = () => {
                 className="error-page-image"
                 />
             <h1 className="text-config">404 page not found</h1>
-            <Button variant="info">
-                <i className="fas fa-home icon-config"></i>Go to Home Page
-            </Button>
+            <Route render={({ history}) => (
+                <Button variant="info" onClick={() => { history.push('/latest') }}>
+                    <i className="fas fa-home icon-config"></i>Go to Home Page
+                </Button>
+            )} />
         </div>
     )
 }
