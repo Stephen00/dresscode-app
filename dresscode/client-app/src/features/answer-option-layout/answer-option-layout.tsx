0beefcbd7d66305e076d69852e329ee35cd6c92b
Martina Borisova <2382419b@student.gla.ac.uk>
2021-03-01 16:42:47 +0000

    Extracted answer option into a separate component
     * Component can be reused for poll and quiz options
     * Quiz answering mechanism NOT FULLY TESTED

---
+++ dresscode/client-app/src/features/answer-option-layout/answer-option-layout.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "./answer-option-layout.css";
+import { Col, Row, Button } from "react-bootstrap";
+
+interface AnswerOptionProps {
+  index: number;
+  postType: string;
+  isAnswered: boolean;
+  option: string;
+  onOptionSelected: Function;
+  optionCounts?: number | undefined;
+  totalVotes?: number | undefined;
+  isCorrectAnswer?: boolean;
+}
+
+const AnswerOption: React.FC<AnswerOptionProps> = ({
+  index,
+  postType,
+  isAnswered,
+  option,
+  optionCounts,
+  onOptionSelected,
+  totalVotes,
+  isCorrectAnswer,
+}) => {
+  const [chosenOption, setChosenOption] = useState<boolean>(false);
+  const [hoveringOver, setHoveringOver] = useState<boolean>(false);
+
+  function handleOptionChosen() {
+    if (!isAnswered) {
+      onOptionSelected(index);
+      setChosenOption(true);
+    }
+  }
+
+  return (
+    <div key={index} aria-disabled={isAnswered}>
+      <Row className="option-row">
+        <Col xs={2} className="option-button-column">
+          <Button
+            className="option-checkbox"
+            onClick={() => handleOptionChosen()}
+          >
+            <i
+              className={`fas fa-check fa-2x ${
+                hoveringOver ? "hovering" : ""
+              }  ${chosenOption ? "chosen" : ""} ${
+                postType === "quiz" &&
+                isAnswered &&
+                isCorrectAnswer &&
+                isCorrectAnswer
+                  ? "correct-quiz-answer"
+                  : ""
+              }`}
+            ></i>
+            <i
+              className="fas fa-times fa-2x"
+              hidden={
+                !(
+                  postType === "quiz" &&
+                  isAnswered &&
+                  isCorrectAnswer &&
+                  !isCorrectAnswer
+                )
+              }
+            ></i>
+          </Button>
+        </Col>
+        <Col
+          xs={7}
+          onMouseEnter={() => setHoveringOver(true)}
+          onMouseLeave={() => setHoveringOver(false)}
+          onClick={() => handleOptionChosen()}
+          className={`option-answer-column ${
+            hoveringOver ? "hovering-text" : ""
+          }`}
+        >
+          {option}
+        </Col>
+        {postType === "poll" &&
+          isAnswered &&
+          optionCounts !== undefined &&
+          totalVotes !== undefined && (
+            <Col xs={3} className="option-count-column">
+              {((optionCounts / totalVotes) * 100).toFixed(0)}%
+            </Col>
+          )}
+      </Row>
+    </div>
+  );
+};
+
+export default AnswerOption;
--- dresscode/client-app/src/features/discover-poll/discover-poll.tsx
+++ dresscode/client-app/src/features/discover-poll/discover-poll.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./discover-poll.css";
-import { Card, Col, Row, Button } from "react-bootstrap";
-import { propTypes } from "react-bootstrap/esm/Image";
 import { PollComponentProps } from "../../views/commonProps";
+import AnswerOption from "../answer-option-layout/answer-option-layout";
 
 const DiscoverPoll: React.FC<PollComponentProps> = ({ poll }) => {
   const answers = [
@@ -13,65 +12,54 @@ const DiscoverPoll: React.FC<PollComponentProps> = ({ poll }) => {
     poll.answer5,
   ].filter(Boolean);
 
-//Filter initialVotes by checking if they are Null or not instead of filtering by Boolean. 
-//Filtering by boolean doesn't work because 0 is Falsy so Boolean(0) returns False in JS and TS
+  //Filter initialVotes by checking if they are Null or not instead of filtering by Boolean.
+  //Filtering by boolean doesn't work because 0 is Falsy so Boolean(0) returns False in JS and TS
   var initialVotes = [
     poll.vote1,
     poll.vote2,
     poll.vote3,
     poll.vote4,
-	poll.vote5,
+    poll.vote5,
   ].filter(function (vote) {
-		return vote!==undefined && vote != null;
-	});
+    return vote !== undefined && vote != null;
+  });
 
   var initialTotalVotes = initialVotes.reduce((a, b) => a!! + b!!, 0);
 
-  const [visible, setVisible] = useState<Boolean>(false);
+  const [visible, setVisible] = useState<boolean>(false);
   const [votes, setVotes] = useState<(number | undefined)[]>(initialVotes);
   const [totalVotes, setTotalVotes] = useState<number | undefined>(
     initialTotalVotes
   );
 
+  function onVotesChange(index: number) {
+    let items: (number | undefined)[] = [...votes];
+    if (items[index] !== undefined) {
+      items[index] = items[index]!! + 1;
+    }
+    setVotes(items);
+    setTotalVotes(totalVotes!! + 1);
+    setVisible(true);
+  }
+
   const items: any = [];
-  answers.forEach((value, index) => {
+  answers.forEach((option, index) => {
     items.push(
-      <div key={index}>
-        <Row className="vote-row">
-          <Col xs={2} className="vote-button-column">
-            <Button
-              className="poll-button"
-              onClick={() => handlePollClick(index)}
-            ></Button>
-          </Col>
-          <Col xs={7} className="vote-answer-column">
-            {value}
-          </Col>
-          <Col xs={3} className="vote-count-column">
-            {visible && (
-              <span>{((votes[index]!! / totalVotes!!) * 100).toFixed(0)}%</span>
-            )}
-          </Col>
-        </Row>
-      </div>
+      <AnswerOption
+        postType="poll"
+        isAnswered={visible}
+        key={index}
+        index={index}
+        option={option!!}
+        optionCounts={votes[index]!!}
+        onOptionSelected={onVotesChange}
+        totalVotes={totalVotes!!}
+      />
     );
   });
 
-  function handlePollClick(index: number) {
-    if (!visible) {
-      let items: (number | undefined)[] = [...votes];
-      if (items[index] !== undefined) {
-        items[index] = items[index]!! + 1;
-      }
-      setVotes(items);
-      setTotalVotes(totalVotes!! + 1);
-
-      setVisible(true);
-    }
-  }
-
   return (
-    <div className="poll-component-section">
+    <div className={`poll-component-section ${visible ? "answered" : ""}`}>
       {items}
       {visible && (
         <div className="after-answer-section">
