0d96e4916010a571c8240912f4544235e1cd8d42
Thanatad Songpetchmongkol <2605415S@student.gla.ac.uk>
2021-03-08 18:25:36 +0700

    search -> redirect to /latest, and show image if no posts found

--- dresscode/client-app/src/views/home-page/home-page.tsx
+++ dresscode/client-app/src/views/home-page/home-page.tsx
@@ -3,10 +3,11 @@ import PostStore from "../../app/stores/postStore";
 import { observer } from "mobx-react-lite";
 import DiscoverCard from "../../features/discover-card/discover-card";
 import "./home-page.css";
+import CatPicture from "../../assets/cat.png";
 
 const HomePage: React.FC = () => {
   const postStore = useContext(PostStore);
-  const { loadAllPosts, posts, removeAllPosts, filteredPosts } = postStore;
+  const { loadAllPosts, removeAllPosts, filteredPosts } = postStore;
 
   useEffect(() => {
     if (!filteredPosts) {
@@ -18,11 +19,27 @@ const HomePage: React.FC = () => {
   }, []);
 
   return (
-    <Fragment key="homepage">
-      {filteredPosts?.map((post) => (
-          <DiscoverCard post={post} key={post.id} />
-      ))}
-    </Fragment>
+    <div>
+      {filteredPosts?.length ? 
+        <div>
+          <Fragment key="homepage">
+          {filteredPosts?.map((post) => (
+              <DiscoverCard post={post} key={post.id} />
+          ))}
+        </Fragment>
+        </div>
+        :
+        <div className="empty-section-config">
+          <img
+            src={CatPicture}
+            alt="no picture found"
+            className="empty-section-image"
+          />
+          <h1 className="text-config">No Post Found</h1>
+        </div>
+      }
+    </div>
+    
   );
 };
 
